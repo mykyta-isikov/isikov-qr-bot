d7521266ff3966c4d1e4ac353159b2bbfe29df7d
Mykyta Isikov <mykyta.isikov@gmail.com>
2021-07-06 18:51:45 +0300

    dev mode, security, code cleaning

--- index.js
+++ index.js
@@ -1,49 +1,43 @@
 const TelegramBot = require('node-telegram-bot-api')
-const express = require('express')
-const Router = require('express')
-
-//const app = express()
+require('dotenv').config()
 
 const PORT = process.env.PORT || 4000
-const TOKEN = '1759647109:AAFuRJPze16vsCTVfg7b-DBb9Bmf2_-VY4g'
+const TOKEN = process.env.TOKEN
+console.log(TOKEN)
 const URL = 'https://isikov-qr-bot.herokuapp.com/bot'
-const bot = new TelegramBot(TOKEN, {
-  webHook: {
-    port: PORT
-  }
-})
+const ISOstring = ' !"#$%&\'()*+,-.0123456789:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[]\\^_`abcdefghijklmnopqrstuvwxyz{}|~@/'
 
-bot.setWebHook(`${URL}/bot${TOKEN}`)
+/// Bot creation
+let bot = undefined
+if(process.env.NODE_ENV == 'production') {
+  bot = new TelegramBot(TOKEN, {
+    webHook: {
+      port: PORT
+    }
+  })
+  bot.setWebHook(`${URL}/bot${TOKEN}`)
 
+  console.log('Bot is running in production mode...')
+} else {
+  bot = new TelegramBot(TOKEN, {polling: true})
+  bot.on('polling_error', console.log)
 
-const ISOstring = ' !"#$%&\'()*+,-.0123456789:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~@/'
-/*
+  console.log('Bot is running  in develop mode...')
+}
 
-const router = Router()
-router.post('/bot', (req) => {
-  bot.processUpdate(req)
-  req.status = 200
-})
-
-app.use(express.urlencoded({extended: true}))
-app.use(router)
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`App is listaning on port ${PORT}`)
-})
-*/
-
-function checkInput(input) {
+/// Functions
+function checkInput(input, patternString) {
   let flag = true
   Array.from(input).forEach(char => {
     let sw = false
-    Array.from(ISOstring).forEach(element => {
+    Array.from(patternString).forEach(element => {
       if(char == element) {
         sw = true
       }
     })
     if(sw == false) {
       flag = false
+      return
     } 
   })
   return flag  
@@ -54,19 +48,14 @@ function sendQRCode(id, input) {
   const png_url = `http://api.qrserver.com/v1/create-qr-code/?data=${validInput}`
 
   bot.sendPhoto(id, png_url)
-  .then(() => {
-    bot.sendMessage(id, "✅Your message was successfully encoded!")
-  })
+  .then()
   .catch((error) => {
     bot.sendMessage(id, "❌Oops... Something went wrong during conversion")
     console.log(error)
   })
 }
 
-console.log('Bot is running...')
-
-bot.on('polling_error', console.log);
-
+/// Bot instructions
 bot.onText(/\/start/, (msg) => {
   const id = msg.chat.id
   const text = `<b>Welcome to QR-code generator!</b>\n\nBegin your work with command: /make [text to convert].\nFor example, "/make test conversion" or '/make https://foo.bar/' will return you fully operational QR-codes.\n\nAlso you can check allowed characters using command /info.`
@@ -76,15 +65,23 @@ bot.onText(/\/start/, (msg) => {
 
 bot.onText(/\/info/, (msg) => {
   const id = msg.chat.id
-  const text = `🛑Character restrictions🛑\nWhen using /make command, input string should not contain characters other than these:\n\n📌${ISOstring}📌.\n\nOtherwise, an error will be thrown and bot will not generate a QR-code.`
+  const text = `Character restrictions\n\nWhen using /make command, input string should not contain characters other than these:\n📌${ISOstring}.\n\nOtherwise, an error will be thrown and bot will not generate a QR-code.`
 
   bot.sendMessage(id, text)
 })
 
+bot.onText(/\/make/, (msg) => {
+  const id = msg.chat.id
+
+  if(msg.text.trim() == '/make') {
+    bot.sendMessage(id, "Please, provide data for encryption")
+  }
+})
+
 bot.onText(/\/make (.+)/, (msg, [raw, text]) => {
   const id = msg.chat.id
-  
-  if(checkInput(text)) {
+
+  if(checkInput(text, ISOstring)) {
     sendQRCode(id, text)
   } else {
     bot.sendMessage(id, 
